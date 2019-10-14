3bd273fa036491996f2afdc7abb12ce63d10ffde
oleg <hnapovskiy20@gmail.com>
2019-10-14 18:16:59 +0300

    Updates

---
+++ precache-manifest.14407cf895dded20289f464d36f9101c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbb0298d298c3f3aac193cbdb86d87a6",
+    "url": "/goit-react-hw-05-budget-planner/index.html"
+  },
+  {
+    "revision": "f7d76bb6c071c2c8c9fb",
+    "url": "/goit-react-hw-05-budget-planner/static/css/main.67e7994e.chunk.css"
+  },
+  {
+    "revision": "1d2b6a470650cb18b6e1",
+    "url": "/goit-react-hw-05-budget-planner/static/js/2.318087ab.chunk.js"
+  },
+  {
+    "revision": "f7d76bb6c071c2c8c9fb",
+    "url": "/goit-react-hw-05-budget-planner/static/js/main.687fe320.chunk.js"
+  },
+  {
+    "revision": "cfb0d3bf37adc150ddda",
+    "url": "/goit-react-hw-05-budget-planner/static/js/runtime-main.bc1a32af.js"
+  }
+]);
